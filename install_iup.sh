e0024d69ba31cc9fe5a39db267e363af349e300e
batiati <rbatiati@gmail.com>
2024-01-12 18:14:53 -0300

    Follow redirect downloading IUP

--- install_iup.sh
+++ install_iup.sh
@@ -3,14 +3,14 @@ set -eu
 
 LIB_DIR=lib
 
-IUP_VERSION="3.30"
+IUP_VERSION="3.31"
 IUP_PLATFORM="Linux54_64"
 IUP_ZIP=iup-"$IUP_VERSION"_"$IUP_PLATFORM"_lib.tar.gz
 
 echo "Downloading $IUP_ZIP..."
 rm -rf $LIB_DIR/iup
 mkdir -p $LIB_DIR/iup && cd $LIB_DIR/iup
-curl https://ufpr.dl.sourceforge.net/project/iup/$IUP_VERSION/Linux%20Libraries/$IUP_ZIP --output $IUP_ZIP
+curl https://ufpr.dl.sourceforge.net/project/iup/$IUP_VERSION/Linux%20Libraries/$IUP_ZIP --location --output $IUP_ZIP
 echo "Extracting $IUP_ZIP..."
 tar -xf "$IUP_ZIP"
 rm "$IUP_ZIP"
@@ -28,7 +28,8 @@ IM_ZIP=im-"$IM_VERSION"_"$IM_PLATFORM"_lib.tar.gz
 echo "Downloading $IM_ZIP..."
 rm -rf $LIB_DIR/im
 mkdir -p $LIB_DIR/im && cd $LIB_DIR/im
-curl https://ufpr.dl.sourceforge.net/project/imtoolkit/$IM_VERSION/Linux%20Libraries/$IM_ZIP --output $IM_ZIP
+echo https://ufpr.dl.sourceforge.net/project/imtoolkit/$IM_VERSION/Linux%20Libraries/$IM_ZIP 
+curl https://ufpr.dl.sourceforge.net/project/imtoolkit/$IM_VERSION/Linux%20Libraries/$IM_ZIP --location --output $IM_ZIP
 echo "Extracting $IM_ZIP..."
 tar -xf "$IM_ZIP"
 rm "$IM_ZIP"
@@ -46,7 +47,7 @@ CD_ZIP=cd-"$CD_VERSION"_"$CD_PLATFORM"_lib.tar.gz
 echo "Downloading $CD_ZIP..."
 rm -rf $LIB_DIR/cd
 mkdir -p $LIB_DIR/cd && cd $LIB_DIR/cd
-curl https://ufpr.dl.sourceforge.net/project/canvasdraw/$CD_VERSION/Linux%20Libraries/$CD_ZIP --output $CD_ZIP
+curl https://ufpr.dl.sourceforge.net/project/canvasdraw/$CD_VERSION/Linux%20Libraries/$CD_ZIP --location --output $CD_ZIP
 echo "Extracting $CD_ZIP..."
 tar -xf "$CD_ZIP"
 rm "$CD_ZIP"
